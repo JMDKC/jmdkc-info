881a77fb04787c762b2e2725f28813f76cfa40d3
JMDKC <josephmathewclift@gmail.com>
2025-04-20 12:35:09 +0100

    updated art table

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,8 +2,8 @@ import csv
 import json
 
 # Define the file paths
-csv_file_path = 'booksload.csv'
-json_file_path = 'books.json'
+csv_file_path = 'artload.csv'
+json_file_path = 'art.json'
 
 # Read the CSV and add data to a list
 data = []
